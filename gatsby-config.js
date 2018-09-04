929f106783d3734fdc00ccb14f7a1eef2f34f3f7
Tom Pichaud <tompichaud@me.com>
2018-09-04 16:02:04 +0200

    postcss not needed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,3 @@
 module.exports = {
-	plugins: [
-		{
-			resolve: 'gatsby-plugin-postcss',
-			options: {
-				postCssPlugins: [require('tailwindcss')('./tailwind.config.js')]
-			}
-		},
-		'gatsby-plugin-emotion'
-	]
+	plugins: ['gatsby-plugin-emotion']
 }
